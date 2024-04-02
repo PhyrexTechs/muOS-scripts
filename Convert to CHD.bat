39658118ef1de49b5bfc8834b529bce2ffde8691
Phyrex Techs <HenryAveryDev@gmail.com>
2024-04-02 04:11:08 -0500

    Update Convert to CHD.bat

--- Convert to CHD.bat
+++ Convert to CHD.bat
@@ -5,8 +5,8 @@ echo Created by: PhyrexTechs
 echo ----------------------------------
 
 
-REM convert cue/iso/bin/gdi files in all subfolders using chdman with cdzl cdfl. 
+REM convert cue/iso/bin/gdi files in current folder and all subfolders using chdman with cdzl cdfl. 
 for /R %%i in (*.cue, *.iso, *.gdi) do chdman createcd -c cdzl,cdfl -i "%%i" -o "%%~dpi/%%~ni.chd"
 
-REM if .chd are present delete cue/iso/bin/gdi files in all subfolders
+REM if .chd are present delete cue/iso/bin/gdi files in in current folder and all subfolders
 for /R %%i in (*.cue, *.iso, *.bin, *.gdi) do for %%j in ("%%~dpi.") do if exist "%%~dpi*.chd" DEL /F "%%~dpi%%~nxi"
